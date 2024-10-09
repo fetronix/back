0c03d7dac91c8039fba61f1888d60f1167ec3927
hacks254 <fnzomo@kenet.or.ke>
2024-10-09 09:13:44 +0300

    added new fields

--- qyfy/admin.py
+++ qyfy/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
-from django.contrib import admin
-from .models import Assets
+class AssetsAdmin(admin.ModelAdmin):
+    # Fields to display in the list view
+    list_display = ('asset_description','category','person_receiving', 'serial_number', 'kenet_tag', 'location', 'status', 'date_received','new_location')
+    
+    # Fields to search for in the admin interface
+    search_fields = ('serial_number', 'kenet_tag', 'asset_description', 'location')
+    
+    # Fields that can be edited directly from the list view
+    list_editable = ('status',)
 
-admin.site.register(Assets)
+    # Filters to filter the data by status or date received
+    list_filter = ('status', 'date_received')
 
+# Register the Assets model with the custom admin class
+admin.site.register(Assets, AssetsAdmin)
+admin.site.register(Category)
--- qyfy/models.py
+++ qyfy/models.py
@@ -1,15 +1,47 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 
 class Assets(models.Model):
+    STATUS_CHOICES = [
+        ('instore', 'In Store'),
+        ('tested', 'Tested'),
+        ('default', 'Default'),
+        ('onsite', 'On Site'),
+        ('pending_release', 'Pending Release'),
+    ]
+
     date_received = models.DateField(auto_now_add=True)  # Automatically set to the current date
     person_receiving = models.CharField(max_length=100)
     asset_description = models.TextField()
     serial_number = models.CharField(max_length=100, unique=True)
     kenet_tag = models.CharField(max_length=100, unique=True)
     location = models.CharField(max_length=200)
+    
+    # Status field with choices and default value
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='instore',  # Default to 'instore'
+        blank=True,
+        null=True
+    )
+
+    # New location field with blank, null, and default value
+    new_location = models.CharField(
+        max_length=200,
+        blank=True,
+        null=True,
+        default='University of Nairobi KENET STORE'
+    )
+    
+    # Foreign key to Category model
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,blank=True,null=True)
 
     def __str__(self):
         return f"{self.asset_description} ({self.serial_number})"
