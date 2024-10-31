3e6add35e5d8f2e7bc19bc9cb75bd4bc917b66f8
hacks254 <fnzomo@kenet.or.ke>
2024-10-31 13:39:05 +0300

    edits

--- core/urls.py
+++ core/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-admin.site.site_header = 'KENET Assets'                    # default: "Django Administration"
+# admin.site.site_header = '.'                    # default: "Django Administration"
 admin.site.index_title = 'Assets Management Area '                 # default: "Site administration"
 admin.site.site_title = 'KENET admin panel' # default: "Django site admin"
 
