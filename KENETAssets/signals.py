db3d1932b6639416ec964f5bb62aaf7ed1d7e55d
hacks254 <fnzomo@kenet.or.ke>
2024-11-07 11:08:28 +0300

    updates

--- KENETAssets/signals.py
+++
@@ -1,9 +0,0 @@
-# signals.py
-from django.db.models.signals import post_migrate
-from django.dispatch import receiver
-from .tasks import remove_expired_cart_items
-
-@receiver(post_migrate)
-def schedule_task(sender, **kwargs):
-    # Schedule the task after the app is ready
-    remove_expired_cart_items(repeat=10)
