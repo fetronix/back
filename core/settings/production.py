d3f71b03f067ffe224cc4191869d0e5f964fd8a7
hacks254 <fnzomo@kenet.or.ke>
2024-11-08 10:53:39 +0300

    updates

---
+++ core/settings/production.py
@@ -0,0 +1,18 @@
+# production.py
+
+from .base import *
+import os
+
+DEBUG = False
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
+    }
+}
