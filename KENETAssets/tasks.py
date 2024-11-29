ef2311f7c84be385a2fd22162b8dd9af4ab2cf8d
hacks254 <fnzomo@kenet.or.ke>
2024-11-29 11:22:59 +0300

    edits

--- KENETAssets/tasks.py
+++ KENETAssets/tasks.py
@@ -4,11 +4,20 @@ from datetime import timedelta
 from .models import Cart, Checkout, Assets
 
 # Task 1: Remove expired cart items with 'pending_release' status
-@background(schedule=10)  # You can adjust the schedule time based on your needs
+@background(schedule=60)  # Default schedule to check periodically, e.g., every minute
 def remove_expired_cart_items():
-    expiration_time = timezone.now() - timedelta(seconds=30)
-    
-    expired_cart_items = Cart.objects.filter(asset__status='pending_release', added_at__lt=expiration_time)
+    # Get the current time and the next midnight
+    now = timezone.now()
+    midnight = (now + timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0)
+
+    # Calculate expiration time (for items added before today)
+    expiration_time = now.replace(hour=0, minute=0, second=0, microsecond=0)
+
+    # Query for expired cart items
+    expired_cart_items = Cart.objects.filter(
+        asset__status='pending_release',
+        added_at__lt=expiration_time
+    )
     
     for cart_item in expired_cart_items:
         # Update asset status to 'instore' before deleting from the cart
@@ -21,6 +30,9 @@ def remove_expired_cart_items():
 
         print(f"Removed expired cart item: {asset.serial_number} for user: {cart_item.user.username}")
 
+    print(f"Scheduled next run for: {midnight}")
+
+
 # Task 2: Process rejected assets, revert status, and remove from checkout
 @background(schedule=10)  # Adjust the schedule as needed
 def process_rejected_cart_items():
@@ -31,7 +43,7 @@ def process_rejected_cart_items():
 
         for cart_item in cart_items:
             asset = cart_item.asset
-            asset.new_location = None
+            asset.destination_location = None
             asset.status = 'instore'
             asset.save()
 
--- KENETAssets/urls.py
+++ KENETAssets/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import *
+from .erp import *
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet)
